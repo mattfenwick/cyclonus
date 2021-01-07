0d613f153cd265493346a13cb9f2c63aa32233db
Matthew Fenwick <mfenwick100@gmail.com>
2021-01-07 12:48:40 -0500

    add traffic query command

--- pkg/cli/netpol-explainer.go
+++ pkg/cli/netpol-explainer.go
@@ -11,6 +11,7 @@ import (
 	"github.com/pkg/errors"
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
+	"io/ioutil"
 	v1 "k8s.io/api/core/v1"
 	networkingv1 "k8s.io/api/networking/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -43,10 +44,11 @@ func setupRootCommand() *cobra.Command {
 	command.PersistentFlags().StringVarP(&flags.Verbosity, "verbosity", "v", "info", "log level; one of [info, debug, trace, warn, error, fatal, panic]")
 
 	command.AddCommand(setupAnalyzePoliciesCommand())
+	command.AddCommand(setupQueryTrafficCommand())
+
 	// TODO
 	//command.AddCommand(setupQueryTargetsCommand())
 	//command.AddCommand(setupQueryPeersCommand())
-	//command.AddCommand(setupQueryTrafficCommand())
 
 	return command
 }
@@ -84,6 +86,59 @@ func runAnalyzePoliciesCommand(args *AnalyzePoliciesArgs) {
 	explainedPolicies := matcher.BuildNetworkPolicies(kubePolicies)
 	printJSON(explainedPolicies)
 	fmt.Printf("%s\n\n", matcher.Explain(explainedPolicies))
+}
+
+type QueryTrafficArgs struct {
+	PolicySource string
+	Namespaces   []string
+	TrafficFile  string
+}
+
+func setupQueryTrafficCommand() *cobra.Command {
+	args := &QueryTrafficArgs{}
+
+	command := &cobra.Command{
+		Use:   "traffic",
+		Short: "query traffic allow/deny",
+		Long:  "given policies and traffic as input, determine whether the traffic would be allowed",
+		Args:  cobra.ExactArgs(0),
+		Run: func(cmd *cobra.Command, as []string) {
+			runQueryTrafficCommand(args)
+		},
+	}
+
+	command.Flags().StringVar(&args.PolicySource, "policy-source", "kube", "source of network policies (kube, file, examples)")
+
+	command.Flags().StringSliceVar(&args.Namespaces, "namespaces", []string{}, "only set if policy-source = kube; selects namespaces to read policies from; leaving empty will select all namespaces")
+
+	command.Flags().StringVar(&args.TrafficFile, "traffic-file", "", "path to traffic file, containing a list of traffic objects")
+	command.MarkFlagRequired("traffic-file")
+
+	return command
+}
+
+func runQueryTrafficCommand(args *QueryTrafficArgs) {
+	// 1. source of policies
+	kubePolicies, err := readPolicies(args.PolicySource, args.Namespaces)
+	utils.DoOrDie(err)
+
+	// 2. consume policies
+	explainedPolicies := matcher.BuildNetworkPolicies(kubePolicies)
+
+	// 3. query
+	var allTraffics []*matcher.Traffic
+	allTrafficBytes, err := ioutil.ReadFile(args.TrafficFile)
+	utils.DoOrDie(err)
+	err = json.Unmarshal(allTrafficBytes, &allTraffics)
+	utils.DoOrDie(err)
+	for _, traffic := range allTraffics {
+		trafficBytes, err := json.MarshalIndent(traffic, "", "  ")
+		utils.DoOrDie(err)
+		result := explainedPolicies.IsTrafficAllowed(traffic)
+		resultBytes, err := json.MarshalIndent(result, "", "  ")
+		utils.DoOrDie(err)
+		fmt.Printf("Traffic:\n%s\n\nIs allowed: %t\n\nExplanation:\n\n%s\n\n", string(trafficBytes), result.IsAllowed(), string(resultBytes))
+	}
 }
 
 func readPolicies(source string, namespaces []string) ([]*networkingv1.NetworkPolicy, error) {
