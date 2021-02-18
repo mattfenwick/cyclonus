0e2b6c9a74946b228a38579c210e38923bf826ad
Matthew Fenwick <mfenwick100@gmail.com>
2021-02-18 15:10:33 -0500

    add cyclonus-worker build script

---
+++ cmd/worker/build.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -eou pipefail
+set -xv
+
+CLUSTER=${CLUSTER:-netpol-calico}
+IMAGE=mfenwick100/cyclonus-worker:latest
+
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o worker ./main.go
+
+docker build -t $IMAGE .
+
+kind load docker-image $IMAGE --name "$CLUSTER"
