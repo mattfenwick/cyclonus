5eb55263eb19c459cbc3d003832337b86c125646
Matthew Fenwick <mfenwick100@gmail.com>
2021-03-03 20:34:20 -0500

    fix up examples

--- examples/run.sh
+++ examples/run.sh
@@ -6,35 +6,35 @@ set -xv
 # run all 5
 go run ../cmd/cyclonus/main.go analyze \
   --use-example-policies \
-  --lint=true \
+  --mode explain,lint,query-target,query-traffic,probe \
   --target-pod-path ./targets-example.json \
   --traffic-path ./traffic-example.json \
   --probe-path ./probe-example.json
 
 # run just the explainer
 go run ../cmd/cyclonus/main.go analyze \
+  --mode explain \
   --policy-path ../networkpolicies/simple-example/
 
 # run just the targets
 go run ../cmd/cyclonus/main.go analyze \
-  --explain=false \
+  --mode query-target \
   --policy-path ../networkpolicies/simple-example/ \
   --target-pod-path ./targets.json
 
 # run just the traffic
 go run ../cmd/cyclonus/main.go analyze \
-  --explain=false \
+  --mode query-traffic \
   --policy-path ../networkpolicies/simple-example/ \
   --traffic-path ./traffic.json
 
 # run just the probe
 go run ../cmd/cyclonus/main.go analyze \
-  --explain=false \
+  --mode probe \
   --policy-path ../networkpolicies/simple-example/ \
   --probe-path ./probe.json
 
 # run just the linter
 go run ../cmd/cyclonus/main.go analyze \
-  --explain=false \
-  --lint=true \
+  --mode lint \
   --policy-path ../networkpolicies/simple-example
