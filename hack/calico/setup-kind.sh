50def9cb3f31479dd1fa4b63999c40379502e7c0
Matthew Fenwick <mfenwick100@gmail.com>
2021-03-12 06:30:02 -0500

    wait for calico-node pods to be ready

--- hack/calico/setup-kind.sh
+++ hack/calico/setup-kind.sh
@@ -17,9 +17,6 @@ kubectl get pods
 kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml
 kubectl -n kube-system set env daemonset/calico-node FELIX_IGNORELOOSERPF=true
 kubectl -n kube-system set env daemonset/calico-node FELIX_XDPENABLED=false
-sleep 5 ; kubectl -n kube-system get pods | grep calico-node
-echo "will wait for calico to start running now... "
-while true ; do
-    kubectl -n kube-system get pods
-    sleep 3
-done
+
+
+kubectl wait --for=condition=ready pod -l k8s-app=calico-node -n kube-system
