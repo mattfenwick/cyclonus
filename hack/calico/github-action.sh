8b6be7e852e4412ca619cb6b3edb76205b97f88c
Matthew Fenwick <mfenwick100@gmail.com>
2021-03-13 08:08:06 -0500

    dedupe github actions

--- hack/calico/github-action.sh
+++ hack/calico/github-action.sh
@@ -3,28 +3,8 @@
 set -eou pipefail
 set -xv
 
-JOB_NAME=job.batch/cyclonus
-JOB_NS=netpol
 CLUSTER_NAME=kind-calico
 
 CLUSTER=$CLUSTER_NAME ./setup-kind.sh
 
-docker pull mfenwick100/cyclonus:latest
-kind load docker-image mfenwick100/cyclonus:latest --name $CLUSTER_NAME
-#
-docker pull k8s.gcr.io/e2e-test-images/agnhost:2.28
-kind load docker-image k8s.gcr.io/e2e-test-images/agnhost:2.28 --name $CLUSTER_NAME
-
-# get some debug info
-kubectl get nodes
-kubectl get pods -A
-
-../run-cyclonus-job.sh ./cyclonus-job-github-action.yaml
-
-# wait for job to come up
-sleep 5
-kubectl get all -A
-
-kubectl wait --for=condition=ready pod -l job-name=cyclonus -n netpol --timeout=5m
-
-kubectl logs -f -n $JOB_NS $JOB_NAME
+JOB_YAML=./cyclonus-job-github-action.yaml CLUSTER_NAME=$CLUSTER_NAME ../run-cyclonus-job.sh
--- hack/cilium/github-action.sh
+++ hack/cilium/github-action.sh
@@ -3,29 +3,8 @@
 set -eou pipefail
 set -xv
 
-JOB_NAME=job.batch/cyclonus
-JOB_NS=netpol
 CLUSTER_NAME=kind-cilium
 
 CLUSTER=$CLUSTER_NAME ./setup-kind.sh
 
-docker pull mfenwick100/cyclonus:latest
-kind load docker-image mfenwick100/cyclonus:latest --name $CLUSTER_NAME
-#
-docker pull k8s.gcr.io/e2e-test-images/agnhost:2.28
-kind load docker-image k8s.gcr.io/e2e-test-images/agnhost:2.28 --name $CLUSTER_NAME
-
-# get some debug info
-kubectl get nodes
-kubectl get pods -A
-
-../run-cyclonus-job.sh ./cyclonus-job-github-action.yaml
-
-# wait for job to start running
-# TODO there's got to be a better way to do this
-sleep 30
-kubectl get all -A
-
-kubectl wait --for=condition=ready pod -l job-name=cyclonus -n netpol --timeout=5m
-
-kubectl logs -f -n $JOB_NS $JOB_NAME
+JOB_YAML=./cyclonus-job-github-action.yaml CLUSTER_NAME=$CLUSTER_NAME ../run-cyclonus-job.sh
