4cfd05e5b691db1c566b58ac7a0258ae66209fa9
Matthew Fenwick <mfenwick100@gmail.com>
2021-04-01 08:54:21 -0400

    first pass at calico/ipv6 kind setup

---
+++ hack/calico-ipv6/setup-kind.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+set -o errexit -o nounset -o pipefail
+set -xv
+
+CLUSTER=${CLUSTER:-netpol-calico-ipv6}
+
+
+kind create cluster --name "$CLUSTER" --config kind-config.yaml
+until kubectl cluster-info;  do
+    echo "$(date)waiting for cluster..."
+    sleep 2
+done
+
+
+kubectl get nodes
+kubectl get all -A
+
+kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml
+kubectl -n kube-system set env daemonset/calico-node FELIX_IGNORELOOSERPF=true
+kubectl -n kube-system set env daemonset/calico-node FELIX_XDPENABLED=false
+kubectl -n kube-system set env daemonset/calico-node FELIX_IPV6SUPPORT=true
+
+kubectl get nodes
+kubectl get all -A
+
+kubectl wait --for=condition=ready nodes --timeout=5m --all
+
+kubectl get nodes
+kubectl get all -A
+
+kubectl wait --for=condition=ready pod -l k8s-app=calico-node -n kube-system
