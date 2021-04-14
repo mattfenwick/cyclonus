c5baf3fdbdd3883b8d6178570d3950572de5307b
Matthew Fenwick <mfenwick100@gmail.com>
2021-04-14 08:14:54 -0400

    fix up cyclonus job args, sonobuoy results signalling

--- cmd/cyclonus/run-sonobuoy-plugin.sh
+++ cmd/cyclonus/run-sonobuoy-plugin.sh
@@ -3,17 +3,17 @@
 set -xv
 set -eou pipefail
 
-CYCLONUS_ARGS=${CYCLONUS_ARGS:-"generate --include conflict --exclude egress,direction"}
-results_dir="${RESULTS_DIR:-/tmp/results}"
+CYCLONUS_ARGS=$@
+RESULTS_DIR="${RESULTS_DIR:-/tmp/results}"
 
 
-./cyclonus $CYCLONUS_ARGS > "${results_dir}"/results.txt
+./cyclonus $CYCLONUS_ARGS > "${RESULTS_DIR}"/results.txt
 
 
-cd "${results_dir}"
+cd "${RESULTS_DIR}"
 
   # Sonobuoy worker expects a tar file.
 tar czf results.tar.gz results.txt
 
 # Signal to the worker that we are done and where to find the results.
-printf "${results_dir}"/results.tar.gz > "${results_dir}"/done
+realpath results.tar.gz > ./done
